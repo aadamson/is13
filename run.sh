0c93b1f801ae18d8f4abe77e7fff336d70841b83
aadamson <aadamson@stanford.edu>
2015-05-09 14:17:35 -0700

    new run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 # !/bin/bash
 source .env/bin/activate
-python examples/deep-example.py
+# THEANO_FLAGS='device=gpu,nvcc.fastmath=True,optimizer_including=local_ultra_fast_sigmoid' python examples/deep-sentence-forward.py -nh 30 -d 3 --emb GoogleNews-vectors-negative300.bin --ex ese_sents.pkl
+THEANO_FLAGS='optimizer_including=local_ultra_fast_sigmoid' python examples/deep-sentence-forward.py -nh 30 -d 3 --emb GoogleNews-vectors-negative300.bin --ex dse_sents.pkl
